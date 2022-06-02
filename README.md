1492cb062154363539388c4cb9ba5cab3758e033
kiligiris <cays1200431@gn.iwasaki.ac.jp>
2022-06-02 13:14:53 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# robocon
+ろぼ魂チームgithubです。
+
+## clone
+```bash
+git clone https://github.com/primary-robocon/robocon  # clone
+```
--- sample.py
+++
@@ -1,8 +0,0 @@
-def f(st, num):
-    sts = st.split()
-    gram = []
-    for i in range(len(sts) - num):
-        gram.append(sts[i:i + num])
-    return gram
-print(f("I am an NLPer", 2))
-print("Hello World")
